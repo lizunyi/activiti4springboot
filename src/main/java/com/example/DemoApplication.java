19915eb58b54d10afd99f164edd400e4e3e691f1
lizunyi <saps@vip.qq.com>
2019-04-26 13:16:01 +0800

    升级activiti 6.0.0

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -1,12 +1,11 @@
 package com.example;
 
-import org.activiti.spring.boot.SecurityAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @EnableTransactionManagement
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@SpringBootApplication
 public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
---
+++ src/main/resources/sql/删除数据.sql
@@ -0,0 +1,15 @@
+delete from leave_apply;
+delete from act_ru_variable;
+delete from act_ru_identitylink;
+delete from act_ru_task;
+delete from act_ru_execution;
+delete from act_hi_varinst;
+delete from act_hi_taskinst;
+delete from act_hi_procinst;
+delete from act_hi_identitylink;
+delete from act_hi_actinst;
+
+delete from act_re_model;
+delete from act_ge_bytearray;
+delete from act_re_deployment;
+delete from act_re_procdef;
